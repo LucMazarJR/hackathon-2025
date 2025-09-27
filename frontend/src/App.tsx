96f3b343b913b2d864c8128e17983675ead9282f
LucianoMazar <lmazaraojr@gmail.com>
2025-09-27 14:25:04 -0300

    feat: pagina do chat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Chat from './pages/Chat'
 import NotFound from './pages/NotFound'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path='/:id/chat' element={<Chat />} />
           <Route path='/login' element={<Login />} />
         </Route>
         <Route path='*' element={<NotFound />} />
